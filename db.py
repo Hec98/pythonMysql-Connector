39440b92b81c053e49d5f1631f7e0d017123070e
Hec98 <hector982015@gmail.com>
2021-07-28 19:35:13 -0500

    Add UPDATE QUERY

--- db.py
+++ db.py
@@ -21,11 +21,17 @@ resultado = cursor.fetchone()
 resultado = cursor.fetchall()
 print(resultado)
 '''
-# Inser data
+''' Inser data
 sql = 'INSERT INTO Usuario (username, email, edad) VALUES (%s, %s, %s)'
 values = ('correoX', 'email@gmail.com', 21)
-
 cursor.execute(sql, values)
 miDb.commit()
 print(cursor.rowcount)
-
+''' 
+# Update data
+sql = 'UPDATE Usuario SET email = %s WHERE id = %s'
+values = ('correoX@gmail.com', 3)
+cursor.execute(sql, values)
+miDb.commit()
+if(cursor.rowcount == 1): print('Update query made')
+else: print('Update query not done')
