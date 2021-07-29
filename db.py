f9e08670b1e7b5e2b0903b6f441b8b6d497053cc
Hec98 <hector982015@gmail.com>
2021-07-28 21:35:37 -0500

    Add DELETE query

--- db.py
+++ db.py
@@ -14,7 +14,6 @@ cursor.execute('SHOW CREATE TABLE Usuario')
 consulta = cursor.fetchall()
 print(consulta)
 '''
-
 ''' Select
 cursor.execute('select * from Usuario')
 resultado = cursor.fetchone()
@@ -28,10 +27,18 @@ cursor.execute(sql, values)
 miDb.commit()
 print(cursor.rowcount)
 ''' 
-# Update data
+''' Update data
 sql = 'UPDATE Usuario SET email = %s WHERE id = %s'
 values = ('correoX@gmail.com', 3)
 cursor.execute(sql, values)
 miDb.commit()
 if(cursor.rowcount == 1): print('Update query made')
 else: print('Update query not done')
+'''
+# Delete data
+sql = 'DELETE FROM Usuario WHERE id = %s'
+values = (3,)
+cursor.execute(sql, values)
+miDb.commit()
+if(cursor.rowcount == 1): print('Delete query made')
+else: print('Delete query not done')
