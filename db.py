0dbe51c6fc3ca3fd3ccd244d03f879fccc7bf9cd
Hec98 <hector982015@gmail.com>
2021-07-29 16:25:53 -0500

    organize code into functions

--- db.py
+++ db.py
@@ -9,43 +9,46 @@ miDb = mysql.connect(
 
 cursor = miDb.cursor()
 
-''' Query database structure
-cursor.execute('SHOW CREATE TABLE Usuario')
-consulta = cursor.fetchall()
-print(consulta)
-'''
-''' Select
-cursor.execute('SELECT * FROM Usuario')
-# resultado = cursor.fetchone()
-resultado = cursor.fetchall()
-print(resultado)
-'''
-''' Inser data
-sql = 'INSERT INTO Usuario (username, email, edad) VALUES (%s, %s, %s)'
-values = ('correoX', 'email@gmail.com', 21)
-cursor.execute(sql, values)
-miDb.commit()
-print(cursor.rowcount)
-''' 
-''' Update data
-sql = 'UPDATE Usuario SET email = %s WHERE id = %s'
-values = ('correoX@gmail.com', 3)
-cursor.execute(sql, values)
-miDb.commit()
-if(cursor.rowcount == 1): print('Update query made')
-else: print('Update query not done')
-'''
-# Delete data
-'''
-sql = 'DELETE FROM Usuario WHERE id = %s'
-values = (3,)
-cursor.execute(sql, values)
-miDb.commit()
-if(cursor.rowcount == 1): print('Delete query made')
-else: print('Delete query not done')
-'''
+def dataBaseStructure():
+    cursor.execute('SHOW CREATE TABLE Usuario')
+    consulta = cursor.fetchall()
+    print(consulta)
 
-cursor.execute('SELECT * FROM Usuario LIMIT 1')
-# resultado = cursor.fetchone()
-resultado = cursor.fetchall()
-print(resultado)
+def selectDataBase():
+    cursor.execute('SELECT * FROM Usuario')
+    # resultado = cursor.fetchone()
+    resultado = cursor.fetchall()
+    print(resultado)
+
+def selectLimit(limit):
+    sql = 'SELECT * FROM Usuario LIMIT %s' 
+    values = (limit,)
+    cursor.execute(sql, values)
+    # resultado = cursor.fetchone()
+    resultado = cursor.fetchall()
+    print(resultado)
+
+def insertData(user, email, edad):
+    sql = 'INSERT INTO Usuario (username, email, edad) VALUES (%s, %s, %s)'
+    values = (user, email, edad)
+    cursor.execute(sql, values)
+    miDb.commit()
+    if(cursor.rowcount == 1): print('Update query made')
+    else: print('Update query not done')
+
+def updateData(idx, user, email, edad):
+    # UPDATE `prueba`.`Usuario` SET `username` = 'user1x', `email` = 'correoXx@gmail.com', `edad` = '19' WHERE (`id` = '1')
+    sql = 'UPDATE Usuario SET username = %s, email = %s, edad = %s WHERE id = %s'
+    values = (user, email, edad, idx)
+    cursor.execute(sql, values)
+    miDb.commit()
+    if(cursor.rowcount == 1): print('Update query made')
+    else: print('Update query not done')
+
+def deleteData(idx):
+    sql = 'DELETE FROM Usuario WHERE id = %s'
+    values = (idx,)
+    cursor.execute(sql, values)
+    miDb.commit()
+    if(cursor.rowcount == 1): print('Delete query made')
+    else: print('Delete query not done')
