9541e8d8c6de2a5e014879f1d0c6ce6a518b1e4b
Héctor De La Torre Jiménez <47239028+Hec98@users.noreply.github.com>
2021-07-29 20:06:39 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -3,8 +3,8 @@
 from simple_term_menu import TerminalMenu
 import time
 
-def pausa(t = 1): 
-    if(t == 0): input('Press a key to continue ')
+def pausa(t = 2): 
+    if(t == 0): input('Press ENTER to continue ')
     else: time.sleep(t)
 
 def menu(title, items):
